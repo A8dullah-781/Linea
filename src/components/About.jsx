caf2d21ffbf877428695448ce6bde0b637a6f48b
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2026-01-07 00:52:50 -0800

    about res

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,27 @@ import Count from "../utils/Count";
 
 const About = () => {
   return (
-    <div className="w-screen h-full px-[5vw] bg-[#FFEBC6]">
-      <div className="uppercase flex justify-between  py-10 items-center">
-        <div className="text-[4.5vw] font-[200]">About&nbsp;&nbsp;Us</div>
-        <div>Harmony, Materiality & Feeling</div>
+    <div className="w-screen h-full md:px-[5vw] pt-3 md:pt-0 px-8 bg-[#FFEBC6]">
+      <div className="uppercase mb-5 flex md:flex-row flex-col justify-between  md:py-10 items-center">
+        <div className="md:text-[4.5vw]  text-[14vw] font-[200]">About&nbsp;&nbsp;Us</div>
+        <div className="md:text-[1.5vw] text-[3vw]">Harmony, Materiality & Feeling</div>
       </div>
 
-      <div className="flex w-full h-1/2 flex-row">
-        <div className="h-full w-[40%]  flex items-center">
+      <div className="flex w-full md:h-1/2 h-full flex-col md:flex-row">
+        <div className="h-full w-full md:w-[40%] hidden md:flex items-center">
           <img className=" rounded-4xl" src="/images/roomOne.jpg" alt="roomOne" />
         </div>
-        <div className="h-full pl-5 flex justify-center mt-10 items-center text-[1.3vw] w-[60%]">
+        <div className="h-full md:pl-5 flex justify-center md:mt-10 items-center text-[2.8vw] text-center md:text-left md:text-[1.3vw] w-full md:w-[60%]">
           <p>Design isn’t just about looks—it’s about creating an experience that resonates with the people who inhabit a space. We specialize in crafting interiors that are not only visually stunning but also highly functional, where every detail is thoughtfully considered and every corner tells a story. Our approach blends creativity with practicality, ensuring that each space is as inspiring to live in as it is to behold.</p>
         </div>
       </div>
 
-      <div className="flex w-full py-15 h-1/2 flex-row">
+      <div className="flex w-full md:py-15 h-full md:h-1/2 flex-col md:flex-row">
         
         <div className="h-full flex flex-col pr-5 text-right gap-10 text-[1.3vw] pl-15 w-[60%]">
-         <div className="-mt-5"> <p>Our 12-member team blends creativity, expertise, and precision. From designers to project managers, we work together to transform ideas into inspiring, functional spaces that leave a lasting impression.</p></div>
+         <div className="-mt-5 hidden md:block"> <p>Our 12-member team blends creativity, expertise, and precision. From designers to project managers, we work together to transform ideas into inspiring, functional spaces that leave a lasting impression.</p></div>
 
-         <div className="flex flex-row justify-evenly items-center">
+         <div className=" hidden md:flex flex-row justify-evenly items-center">
           <div className="flex flex-col justify-center items-center">
           <div className="text-[4.5vw]"><Count target={11} step={1} speed={110}/>+</div>
           <p className="text-[1vw]">Arcitects & Designers</p>
@@ -40,9 +40,26 @@ const About = () => {
 
 
         </div>
-        <div className="h-full w-[40%] flex items-center ">
-          <img className=" rounded-4xl -mt-15 " src="/images/roomTwo.jpg" alt="roomTwo" />
+        <div className="h-full w-full md:w-[40%] py-3 md:py-0 flex items-center ">
+          <img className=" rounded-4xl md:-mt-15 " src="/images/roomTwo.jpg" alt="roomTwo" />
         </div>
+        <div className="md:-mt-5 text-center md:text-left block md:hidden text-[2.8vw] md:text-[1.3vw]"> <p>Our 12-member team blends creativity, expertise, and precision. From designers to project managers, we work together to transform ideas into inspiring, functional spaces that leave a lasting impression.</p></div>
+
+
+        <div className=" flex flex-row md:hidden py-8 justify-evenly items-center">
+          <div className="flex flex-col justify-center items-center">
+          <div className="text-[10vw]"><Count target={11} step={1} speed={110}/>+</div>
+          <p className="text-[2.5vw]">Arcitects & Designers</p>
+         </div>
+         <div className="flex flex-col justify-center items-center">
+          <div className="text-[10vw]"><Count target={130} step={10} speed={70}/>+</div>
+          <p className="text-[2.5vw]">Projects</p>
+         </div>
+         <div className="flex flex-col justify-center items-center">
+          <div className="text-[10vw]"><Count target={5} step={1} speed={140}/>+</div>
+          <p className="text-[2.5vw]">Years Of Experience</p>
+         </div>
+         </div>
       </div>
 
     </div>
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Featured = () => {
   return (
-        <div className='w-full px-[4.5vw] pb-2 gap-2 justify-center items-end bg-[#3E2519] flex h-screen'>
+        <div className='w-full hidden px-[4.5vw] pb-2 gap-2 justify-center items-end bg-[#3E2519] md:flex h-screen'>
             <div className='h-[90%]  w-[40%] bg-[url(/images/featureOne.jpg)] bg-contain'>
             </div>
             <div className='h-[90%] flex flex-col justify-center items-center gap-2 w-[60%]'>
