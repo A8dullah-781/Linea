f0fe5db98ee66eff94a94c40cc0790e33b1cbfeb
A8dullah-781 <hmabdullahfarooq781@gmail.com>
2026-01-09 01:55:15 -0800

    debug 3

--- src/pages/WorkTwo.jsx
+++ src/pages/WorkTwo.jsx
@@ -30,7 +30,7 @@ const WorkTwo = () => {
         <div className=' bg-[url(/images/workTwo/twoFour.webp)] h-[40vh] md:h-[70vh] bg-cover bg-center rounded-2xl w-[90vw]'>
 
         </div>
-        <div className=' bg-[url(/images/workTwo/twoFive.webp)]  h-[40vh] md:h-[70vh] bg-cover bg-bottom rounded-2xl w-[90vw]'>
+        <div className=' bg-[url(/images/workTwo/twoFive.webp)] hidden md:block  h-[40vh] md:h-[70vh] bg-cover bg-bottom rounded-2xl w-[90vw]'>
 
         </div>
     </div>
